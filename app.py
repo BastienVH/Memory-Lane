1f993e2cb57954a11e5d3ab6d72f1bd745f11fd7
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2020-08-10 16:35:56 +0200

    improvement: gallery gets populated by a for loop

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, redirect, render_template, request
+from os import walk
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    filenames = []
+    for dirpath, dirs, files in walk("./static/uploads"):
+        filenames.extend(files)
+        break
+    return render_template('index.html', filenames=filenames)
 
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
