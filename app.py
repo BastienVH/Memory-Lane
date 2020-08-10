d14389b6a90d3cbaea53d51a56b6e5a58b40b237
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2020-08-10 10:29:33 +0200

    Added first version of upload page

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        # Submit file to database
+        redirect('/')
+    else:
+        return render_template('upload.html')
