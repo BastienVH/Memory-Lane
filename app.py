66246920a48e76193409ab92f59aba35510ae5af
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2020-08-12 08:36:37 +0200

    Fix image uploads destination folder

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask, redirect, render_template, request
-from os import walk
+from os import path, walk
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif', '.mov']
+
 @app.route('/')
-def hello():
+def gallery():
     filenames = []
-    for dirpath, dirs, files in walk("./static/uploads"):
+    for dirpath, dirs, files in walk(app.config['UPLOAD_FOLDER']):
         filenames.extend(files)
         break
     filenames.remove("PUT_FILES_HERE")
     return render_template('index.html', filenames=filenames)
 
 @app.route('/upload', methods=["GET", "POST"])
-def upload():
+def upload_files():
     if request.method == "POST":
-        # Submit file to database
-        redirect('/')
+        for uploaded_file in request.files.getlist('file'):
+            if uploaded_file.filename != '':
+                filename = secure_filename(uploaded_file.filename)
+                uploaded_file.save(path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect('/')
     else:
         return render_template('upload.html')
