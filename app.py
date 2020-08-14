3db5c694cabdf13293d14fd6dc4ca59b3f1daa57
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2020-08-14 17:00:40 +0200

    Added setup route (only accessible once)

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, redirect, render_template, request
-from os import path, walk
+from os import path, walk, getenv, environ
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 
 app = Flask(__name__)
 app.config.from_object("config.DevelopmentConfig")
 
+# Setup View (only accessible on first launch)
+@app.route('/setup', methods=['GET', 'POST'])
+def setup():
+    if getenv('SETUP_MODE') != 'Setup_true':
+        return redirect('/')
+    else:
+        if request.method == "POST":
+            # make supplied email admin
+            # change SETUP_MODE to False in .env
+            fin = open(".env", "rt")
+            data = fin.read()
+            data = data.replace('Setup_true', 'Setup_false')
+            fin.close
+            fin = open(".env", "wt")
+            fin.write(data)
+            fin.close()
+            # directly set env SETUP_MODE to false
+            environ['SETUP_MODE'] = 'Setup_false'
+            return 'posting setup'
+        else:
+            return render_template('setup.html')
+
+# Main gallery view
 @app.route('/')
 def gallery():
     filenames = []
@@ -15,6 +38,7 @@ def gallery():
     filenames.remove("PUT_FILES_HERE")
     return render_template('index.html', filenames=filenames)
 
+# Batch upload view
 @app.route('/upload', methods=["GET", "POST"])
 def upload_files():
     if request.method == "POST":
